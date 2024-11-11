fa2cad0882453e91dd369618d22a1452e353ecd7
EnricoGenovese <enricogeno@libero.it>
2024-11-11 17:36:06 +0100

    add creation of array of numbers from user input;

--- js/script.js
+++ js/script.js
@@ -9,25 +9,14 @@ che ha visto precedentemente, nell'ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare 
 sono stati individuati.
 ------------------------------------------------------------------------------------------------ */
-
-// Variabli da elementi HTML
-
 const randomNumbers = document.getElementById("numbers-list");
-
-const sendAnswer = document.getElementById("answers-form");
-
-// Array vuoti per i numeri generati, le risposte dell'utente e i numeri indovinati;
-
-
-let userNumbers = [];
-let numbersGuessed = [];
 
 // Countdown 
 
-let seconds = 10;
+let seconds = 2;
 const timer = setInterval(function () {
     if (seconds > 1) {
-        countdown.innerHTML = --seconds;
+        countdown.innerHTML = seconds--;
     } else {
         countdown.innerHTML = "Inserisci i numeri di Simon"
         instructions.innerHTML = "";
@@ -37,19 +26,68 @@ const timer = setInterval(function () {
     }
 }, 1000);
 
+
+
+// Controllo della corrispondenza dei numeri randomici con i numeri dell'utente;
+// Stampa risultato: quanti numeri indovinati(quali numeri indovinati);
+
+// Array vuoti per i numeri generati, le risposte dell'utente e i numeri indovinati;
+
+const sendAnswer = document.getElementById("answers-form");
+const inputs = document.querySelectorAll("input");
+
+sendAnswer.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let userNumbers = [];
+    let numbersGuessed = [];
+    let counter = 0;
+    for (let i = 0; i < 5; i++) {
+        let input = inputs[i].value;
+        userNumbers.push(parseInt(input));
+        console.log(userNumbers)
+    } for (let i = 0; i < answer.length; i++) {
+    if (inputs.includes(answer[i])) {
+        counter++;
+        numbersGuessed.push(inputs[i]);
+    }
+    console.log(counter);
+    console.log(numbersGuessed);
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+/* FUNZIONI */
+
 // Funzione per creare i <li> che conterranno i numeri casuali
+
+
 
 function createGuess(index) {
     let numbersToGuess = [];
     for (let i = 0; i < index; i++) {
-        const guess = document.createElement("li");
-        numbersToGuess.push(randNumGen(1, 50));
-        guess.innerHTML = numbersToGuess[i];
-        randomNumbers.appendChild(guess)
+        const liEl = document.createElement("li");
+        //
+        const rand = randNumGen(1, 50)
+        liEl.innerHTML = rand;
+        console.log(rand);
+        //
+        randomNumbers.appendChild(liEl);
+        //
+        numbersToGuess.push(rand);
+        console.log(numbersToGuess);
     }
+
+    return numbersToGuess;
 }
 
-createGuess(5)
+let answer = createGuess(5)
 
-
-// 
